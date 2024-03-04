16a19bb06e4d36ff51e4c0ff4d07c62077904082
JeffDemanche <jeffrey_demanche@alumni.brown.edu>
2024-03-03 20:57:28 -0500

    Add ErrorBoundary component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,13 +6,16 @@ import { theme } from "styles/styles";
 import "simplebar-react/dist/simplebar.min.css";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "util/apollo";
+import { ErrorBoundary } from "./ErrorBoundary";
 
 export const App: React.FC = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <ApolloProvider client={client}>
-        <IpsumRouter />
-      </ApolloProvider>
-    </ThemeProvider>
+    <ErrorBoundary>
+      <ThemeProvider theme={theme}>
+        <ApolloProvider client={client}>
+          <IpsumRouter />
+        </ApolloProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
   );
 };
