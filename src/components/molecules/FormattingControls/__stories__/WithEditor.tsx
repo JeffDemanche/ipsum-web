1e3238183b6aa3235592855d9a347ce1bddedbe1
Jeff Demanche <jeffrey_demanche@alumni.brown.edu>
2024-04-17 21:28:31 -0400

    Update Lexical and add highlighting to FormattingControls story

--- src/components/molecules/FormattingControls/__stories__/WithEditor.tsx
+++ src/components/molecules/FormattingControls/__stories__/WithEditor.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 import { EntryEditor } from "../../../atoms/EntryEditor";
 import { FormattingControls } from "../FormattingControls";
 import { FormattingControlsProvider } from "../FormattingControlsContext";
 
 export const WithEditor: React.FunctionComponent = () => {
+  const [highlightsMap, setHighlightsMap] = useState<
+    Record<string, { id: string; hue: number }>
+  >({});
+
   return (
     <FormattingControlsProvider>
       <FormattingControls />
-      <EntryEditor />
+      <EntryEditor
+        highlightsMap={highlightsMap}
+        createHighlight={() => {
+          const id = uuidv4();
+          setHighlightsMap((prev) => ({
+            ...prev,
+            [id]: { id, hue: Math.floor(Math.random() * 360) },
+          }));
+          return id;
+        }}
+      />
     </FormattingControlsProvider>
   );
 };
--- src/components/molecules/FormattingControls/__tests__/FormattingControls.tests.tsx
+++ src/components/molecules/FormattingControls/__tests__/FormattingControls.tests.tsx
@@ -1,4 +1,5 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { EntryEditor } from "components/atoms/EntryEditor";
 import React from "react";
 
@@ -36,5 +37,52 @@ describe("FormattingControls", () => {
     });
 
     expect(createHighlight).toHaveBeenCalled();
+  });
+
+  describe("content highlighting", () => {
+    /**
+     * Lexical with RTL is buggy and the test fails.
+     */
+    it.skip("should highlight the selected text", async () => {
+      const createHighlight = jest.fn(() => "highlight-id");
+      const highlightsMap = {
+        "highlight-id": { id: "highlight-id", hue: 0 },
+      };
+      render(
+        <FormattingControlsProvider>
+          <FormattingControls />
+          <EntryEditor
+            // Lexical is buggy in the RTL environment unless you give the editor some initial content
+            initialHtmlString="<p>It is written:</p>"
+            highlightsMap={highlightsMap}
+            createHighlight={createHighlight}
+          />
+        </FormattingControlsProvider>
+      );
+      const editor = await screen.findByRole("textbox");
+      await waitFor(async () => {
+        await userEvent.click(editor);
+        await userEvent.type(
+          editor,
+          "each step I take feels like a liberation from the shackles of worldly attachments"
+        );
+      });
+      await waitFor(async () => {
+        await userEvent.click(
+          await screen.findByRole("button", { name: "highlight" })
+        );
+        expect(createHighlight).toHaveBeenCalled();
+      });
+
+      expect(createHighlight).toHaveBeenCalled();
+      const span = screen.queryByText(
+        "It is written:each step I take feels like a liberation from the shackles of worldly attachments"
+      );
+      expect(span).toBeInTheDocument();
+      expect(span.parentElement).toHaveAttribute(
+        "data-highlight-id",
+        "highlight-id"
+      );
+    });
   });
 });
