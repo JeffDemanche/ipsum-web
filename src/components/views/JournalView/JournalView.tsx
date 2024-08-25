0117fb6ffcbd45d8e5f8d961b972e9e45c46e83a
Jeff Demanche <jeffrey_demanche@alumni.brown.edu>
2024-08-25 13:58:19 -0400

    Wrap JournalView in FormattingControlsProvider

--- src/components/views/JournalView/JournalView.tsx
+++ src/components/views/JournalView/JournalView.tsx
@@ -1,3 +1,4 @@
+import { FormattingControlsProvider } from "components/molecules/FormattingControls";
 import { BrowserDrawerConnected } from "components/organisms/BrowserDrawer";
 import { JournalSettingsDrawerConnected } from "components/organisms/JournalSettingsDrawer";
 import { JournalViewPagesSectionConnected } from "components/organisms/JournalViewPagesSection";
@@ -12,15 +13,17 @@ export const JournalView: React.FunctionComponent = () => {
 
   return (
     <IpsumStateProvider projectState={mockSiddhartha().projectState}>
-      <div className={styles["journal-view"]}>
-        <JournalSettingsDrawerConnected />
-        <div className={styles["pages-section-container"]}>
-          <JournalViewPagesSectionConnected
-            className={styles["pages-section"]}
-          />
+      <FormattingControlsProvider>
+        <div className={styles["journal-view"]}>
+          <JournalSettingsDrawerConnected />
+          <div className={styles["pages-section-container"]}>
+            <JournalViewPagesSectionConnected
+              className={styles["pages-section"]}
+            />
+          </div>
+          <BrowserDrawerConnected />
         </div>
-        <BrowserDrawerConnected />
-      </div>
+      </FormattingControlsProvider>
     </IpsumStateProvider>
   );
 };
