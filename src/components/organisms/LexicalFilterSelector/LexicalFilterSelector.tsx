55ba95f228314d618453c77458d5582b7c5770b0
Jeff Demanche <jeffrey_demanche@alumni.brown.edu>
2024-09-28 15:55:13 -0400

    Add new filter selector component and install ebnf library

---
+++ src/components/organisms/LexicalFilterSelector/LexicalFilterSelector.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import styles from "./LexicalFilterSelector.less";
+
+export const LexicalFilterSelector: React.FC = () => {
+  return <div />;
+};
---
+++ src/components/organisms/LexicalFilterSelector/index.ts
@@ -0,0 +1,1 @@
+export { LexicalFilterSelector } from "./LexicalFilterSelector";
