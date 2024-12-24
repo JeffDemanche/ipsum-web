6313582b4fd637348afa38320d05dcd6dde998b8
Jeff Demanche <jeffrey_demanche@alumni.brown.edu>
2024-12-24 14:17:01 -0500

    Create and execute migration script

--- scripts/project-tools/migrations/00-comments-migration.ts
+++ scripts/project-tools/migrations/00-comments-migration.ts
@@ -1,4 +1,5 @@
-import { IpsumTimeMachine } from "util/diff";
+import { InMemoryRelation } from "util/state";
+import { v4 as uuid } from "uuid";
 
 // 1. Create incomingRelations on highlights
 // 2. Make sure commentEntries still compatible
@@ -8,11 +9,47 @@ import { IpsumTimeMachine } from "util/diff";
 export default function commentsMigration(data: any) {
   const commentsCopy = { ...data.comments };
 
-  Object.keys(commentsCopy).forEach((comment: any) => {
-    const commentEntry = data.commentEntries[comment.commentEntry];
+  const relationsCopy = { ...data.relations };
 
-    const entry = data.entries[commentEntry.entry];
-
-    const htmlString = IpsumTimeMachine.fromString(entry.trackedHTMLString);
+  // Create incomingRelations on highlights
+  Object.keys(data.highlights).forEach((highlight: any) => {
+    console.log(highlight);
+    data.highlights[highlight].incomingRelations = [];
   });
+
+  Object.keys(commentsCopy).forEach((comment: any) => {
+    const commentData = commentsCopy[comment];
+
+    // Rename "highlight" to "objectHighlight"
+    commentData.objectHighlight = commentData.highlight;
+    delete commentData.highlight;
+
+    console.log(commentData);
+
+    const newRelationId = uuid();
+    const newRelation: InMemoryRelation = {
+      __typename: "Relation",
+      id: newRelationId,
+      objectType: "Highlight",
+      object: commentData.objectHighlight,
+      predicate: "responds to",
+      subject: commentData.id,
+      subjectType: "Comment",
+    };
+
+    relationsCopy[newRelationId] = newRelation;
+
+    // Add "outgoingRelations"
+    commentData.outgoingRelations = [newRelationId];
+
+    data.highlights[commentData.objectHighlight].incomingRelations = [
+      ...data.highlights[commentData.objectHighlight].incomingRelations,
+      newRelationId,
+    ];
+
+    data.comments[comment] = commentData;
+  });
+
+  data.relations = relationsCopy;
+  data.comments = commentsCopy;
 }
